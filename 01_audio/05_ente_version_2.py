dd04f1438fd41ce85c079e9f3f1ecca6d8531f1f
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-03-18 08:22:14 +0100

    Ente mit Variablen

---
+++ 01_audio/05_ente_version_2.py
@@ -0,0 +1,49 @@
+import music as mc
+
+c_1 = 262
+d_1 = 294
+e_1 = 330
+f_1 = 350
+g_1 = 392
+a_1 = 440
+
+viertel_note = 100
+halbe_note = 2 * viertel_note
+ganze_note = 2 * halbe_note
+
+mc.pitch(c_1, viertel_note)
+mc.pitch(d_1, viertel_note)
+mc.pitch(e_1, viertel_note)
+mc.pitch(f_1, viertel_note)
+
+mc.pitch(g_1, halbe_note)
+mc.pitch(g_1, halbe_note)
+
+mc.pitch(a_1, viertel_note)
+mc.pitch(a_1, viertel_note)
+mc.pitch(a_1, viertel_note)
+mc.pitch(a_1, viertel_note)
+
+mc.pitch(g_1, ganze_note)
+
+mc.pitch(a_1, viertel_note)
+mc.pitch(a_1, viertel_note)
+mc.pitch(a_1, viertel_note)
+mc.pitch(a_1, viertel_note)
+
+mc.pitch(g_1, ganze_note)
+
+mc.pitch(f_1, viertel_note)
+mc.pitch(f_1, viertel_note)
+mc.pitch(f_1, viertel_note)
+mc.pitch(f_1, viertel_note)
+
+mc.pitch(e_1, halbe_note)
+mc.pitch(e_1, halbe_note)
+
+mc.pitch(d_1, viertel_note)
+mc.pitch(d_1, viertel_note)
+mc.pitch(d_1, viertel_note)
+mc.pitch(d_1, viertel_note)
+
+mc.pitch(c_1, ganze_note)
