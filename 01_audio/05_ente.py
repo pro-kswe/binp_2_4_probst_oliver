9f6a0232ec74dec5e6b4a023a98639dbd831b26d
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-03-18 08:20:53 +0100

    Ente

---
+++ 01_audio/05_ente.py
@@ -0,0 +1,38 @@
+import music as mc
+
+mc.pitch(262, 400)
+mc.pitch(294, 400)
+mc.pitch(330, 400)
+mc.pitch(350, 400)
+
+mc.pitch(392, 800)
+mc.pitch(392, 800)
+
+mc.pitch(440, 400)
+mc.pitch(440, 400)
+mc.pitch(440, 400)
+mc.pitch(440, 400)
+
+mc.pitch(392, 1600)
+
+mc.pitch(440, 400)
+mc.pitch(440, 400)
+mc.pitch(440, 400)
+mc.pitch(440, 400)
+
+mc.pitch(392, 1600)
+
+mc.pitch(350, 400)
+mc.pitch(350, 400)
+mc.pitch(350, 400)
+mc.pitch(350, 400)
+
+mc.pitch(330, 800)
+mc.pitch(330, 800)
+
+mc.pitch(294, 400)
+mc.pitch(294, 400)
+mc.pitch(294, 400)
+mc.pitch(294, 400)
+
+mc.pitch(262, 1600)
