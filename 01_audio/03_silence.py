a59b978d0d3131b6f23ee2337eff0d853ed46624
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-03-18 08:21:28 +0100

    Silence

---
+++ 01_audio/03_silence.py
@@ -0,0 +1,10 @@
+import music
+import microbit
+
+music.pitch(330, 660)
+microbit.sleep(1000)
+music.pitch(330, 300)
+microbit.sleep(500)
+music.pitch(330, 450)
+microbit.sleep(250)
+music.pitch(330, 150)
