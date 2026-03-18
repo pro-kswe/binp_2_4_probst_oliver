c249c6b6942ba5c8459ce7ee40fe17db0fd7c068
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-03-18 08:21:44 +0100

    SOS

---
+++ 01_audio/04_sos.py
@@ -0,0 +1,24 @@
+import music as mc
+import microbit as mt
+
+mc.pitch(440, 150)
+mt.sleep(100)
+mc.pitch(440, 150)
+mt.sleep(100)
+mc.pitch(440, 150)
+
+mt.sleep(500)
+
+mc.pitch(440, 500)
+mt.sleep(100)
+mc.pitch(440, 500)
+mt.sleep(100)
+mc.pitch(440, 500)
+
+mt.sleep(500)
+
+mc.pitch(440, 150)
+mt.sleep(100)
+mc.pitch(440, 150)
+mt.sleep(100)
+mc.pitch(440, 150)
