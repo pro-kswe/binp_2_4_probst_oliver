b8df5c881f49a283ca35a0b8888f2698ef506d32
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-04-29 08:42:39 +0200

    Beispiel

---
+++ 02_algorithmus/01_beispiel.py
@@ -0,0 +1,9 @@
+wochentage = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
+zahlen = [67, 42, 4, -7, 56, 1024, 65536, 0, -12]
+temperaturen = [25.5, 32.0, -5.5, 10.0]
+abteilungen = ["F2A", 23, "F2B", 23, "F2C", 22]
+
+print(wochentage)
+print(zahlen)
+print(temperaturen)
+print(abteilungen)
