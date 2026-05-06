3a5502182d883d24fb32a5c852b591fa0e307a16
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-05-06 08:44:43 +0200

    Weiter an den Planetenaufgaben

--- 02_algorithmus/02_planeten.py
+++ 02_algorithmus/02_planeten.py
@@ -1,3 +1,5 @@
+import random as rd
+
 planeten = ["Merkur", "Venus", "Erde", "Mars", "Jupiter", "Saturn", "Uranus", "Neptun"]
 print(planeten)
 print(planeten[0])
@@ -8,3 +10,9 @@ print(planeten[4])
 print(planeten[5])
 print(planeten[6])
 print(planeten[7])
+
+for _ in range(5):
+    print("Zufälliger Planet")
+    i = rd.randrange(0, 8)
+    print(planeten[i])
+    
